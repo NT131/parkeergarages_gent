5b945545eaa8d1281e46f0acc904d50bc82f0795
NT_RaspberryPi <niels_tack@hotmail.com>
2024-01-23 15:58:56 +0100

    modify structure of mirroring

--- gunicorn_and_nginx_files/sync_config.sh
+++ gunicorn_and_nginx_files/sync_config.sh
@@ -1,7 +1,7 @@
 # Paths to individual configuration files and the local clone of the GitHub repository
 nginx_conf="/etc/nginx/sites-available/dashapp" # /path/to/nginx/conf
 gunicorn_service="/etc/systemd/system/gunicorn_parkings_ghent.service" # /path/to/gunicorn/service
-GITHUB_REPO_DIR=/home/nielstack/projects/erpohk/visualisations/parkings_ghent/gunicorn_and_nginx_files #"/path/to/local/github/repo"
+GITHUB_REPO_DIR="/home/nielstack/projects/erpohk/visualisations/parkings_ghent/gunicorn_and_nginx_files/mirrored_files" #"/path/to/local/github/repo"
 
 # Copy or sync each specific configuration file to the GitHub repository
 rsync -av --exclude='.git/' "$nginx_conf" "$GITHUB_REPO_DIR/"
